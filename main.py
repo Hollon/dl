bec10cf8db02b5ae132982e7e383241b0d54f628
Hollon <hollon680830@163.com>
2022-11-05 10:12:42 +0800

    add

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi()
-    print("vscode")
+    print("code")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
