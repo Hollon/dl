acd0399b8fcc1ca9cb9df85b5653776fbd4a671f
Hollon <hollon680830@163.com>
2022-11-05 11:19:44 +0800

    hollon

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import torch
 def print_hi():
     # Use a breakpoint in the code line below to debug your script.
     print(torch.cuda.is_available())
-    print(torch.cuda.get_device_name(0))
+   
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
